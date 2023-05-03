8cc045a479427fec67d92394bf9e7705d01d4fb1
black-cypher <basilsimon2@gmail.com>
2023-05-03 13:54:38 +0200

    single round of rock paper scissors

---
+++ main.js
@@ -0,0 +1,31 @@
+function getComputerChoice(){
+    const choices = ['rock', 'paper', 'scissors'];
+
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = prompt('Please choose between "Rock", "Paper" or "Scissors"').toLowerCase();
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        return "Nice! You tied";
+    }
+    else if(playerSelection === 'rock' && computerSelection === 'paper'){
+        return `You Lose! paper beats rock`;
+    }
+    else if(playerSelection === 'paper' && computerSelection === 'scissors'){
+        return `You Lose! scissors beats paper`;
+    }
+    else if(playerSelection === 'scissors' && computerSelection === 'rock'){
+        return `You Lose! rock beats scissors`;
+    }
+    else{
+        return "CONGRATULATIONS! You win";
+    }
+}
+
+console.log(`PLAYER: ${playerSelection}`);
+console.log(`COMPUTER: ${computerSelection}`);
+console.log(playRound(playerSelection, computerSelection));
+
